5e297989064c32750c4aec3344abda1a0f37bfb7
1453141247@qq.com <1453141247@qq.com>
2023-11-07 20:01:29 +0800

    Axios基础配置

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import { getCategory } from "@/apis/testApi";
+getCategory().then(function (res) {
+  console.log(res);
+});
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
